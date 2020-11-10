8e7b5a1e2e127c563d592f869831416e499db4f8
Romain Gericot <romain.gericot@gmail.com>
2020-11-11 00:02:09 +0100

    Add edit and update methods to Flats Controller. Add view for new flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -20,6 +20,16 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
   private
 
   def flat_params

d8738f5372a1abdb92301b5e18cb9879336d8d2b
Romain Gericot <romain.gericot@gmail.com>
2020-11-11 00:04:31 +0100

    Add destroy method to Flats Controller

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -30,6 +30,12 @@ class FlatsController < ApplicationController
     redirect_to flat_path(@flat)
   end
 
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
+  end
+
   private
 
   def flat_params
